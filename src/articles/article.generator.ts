05a799af1e7da45c7bc3dc5e4da591aa111c8e24
Matt Martin <matt.martin0108@gmail.com>
2023-04-10 16:47:12 +0930

    logging on generate article

--- src/articles/article.generator.ts
+++ src/articles/article.generator.ts
@@ -1,5 +1,8 @@
 const { Configuration, OpenAIApi } = require("openai");
 import { trim } from "lodash";
+import { createLogger } from "../services/logger/logger";
+
+const logger = createLogger("ArticleGenerator");
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
@@ -9,12 +12,16 @@ async function generateArticleContent(prompt: string) {
   });
   const openai = new OpenAIApi(config);
 
+  logger.info({ prompt, l: OPENAI_API_KEY.length }, "Generating Article");
+
   const gptResponse = await openai.createCompletion({
     model: "text-davinci-003",
     prompt,
     max_tokens: 200,
     temperature: 1,
   });
+
+  logger.info({ gptResponseDate: gptResponse.data }, "Generated Article");
 
   return trim(gptResponse.data.choices[0].text);
 }
