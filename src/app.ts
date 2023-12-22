bfd3f4648dda589468a7f301a6031e46e2e25a5b
Matt Martin <matt.martin0108@gmail.com>
2023-12-22 22:23:36 +1030

    update cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://alchemy-tech.com"
+        ? "https:/mattmartin.au"
         : "http://localhost:3000",
   })
 );
